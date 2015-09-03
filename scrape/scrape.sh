f66f22172d973a1ade9d1c72a3dec903fb99f0e5
Panos Rontogiannis <p_ronto@hotmail.com>
2015-09-03 15:38:51 +0300

    Added sleep intervals to make it nicer towards the site-to-scrape :)

--- scrape/scrape.sh
+++ scrape/scrape.sh
@@ -17,6 +17,7 @@ FN_ALL_FILMS=$DIR_TMP/all_films.json
 FN_FILM_INFO=$DIR_TMP/films.info
 FN_SHOWTIMES=$DIR_TMP/showtimes.json
 FN_UPDATE_INFO=$DIR_TMP/update_info.json
+SLEEP_BETWEEN_REQUEST=1
 
 function scrape_films
 {
@@ -25,9 +26,10 @@ function scrape_films
     : ${2? "scrape_films: missing second argument"}
     # Scrape the film info from each URL.
     while read film_url
-    do
+    do        
         echo scraping ${URL_CINEMA}${film_url}...
-        phantomjs scrape_film.js ${URL_CINEMA}$film_url >> $2        
+        phantomjs scrape_film.js ${URL_CINEMA}$film_url >> $2
+        sleep $SLEEP_BETWEEN_REQUEST
     done < $1
 }
 
@@ -81,6 +83,7 @@ echo +New arrivals
 # Scraped URLs are stored to a file as well as output to STDOUT.
 echo scraping URLs...
 phantomjs scrape_all_films.js $URL_NEW_ARRIVALS > $FN_NEW_ARRIVAL_URLS
+sleep $SLEEP_BETWEEN_REQUEST
 scrape_films $FN_NEW_ARRIVAL_URLS $FN_NEW_ARRIVALS
 
 # Scrape all films.
@@ -88,27 +91,40 @@ echo +All films
 # Scraped URLs are stored to a file as well as output to STDOUT.
 echo scraping URLs...
 phantomjs scrape_all_films.js $URL_ALL_FILMS > $FN_ALL_FILMS_URLS
+sleep $SLEEP_BETWEEN_REQUEST
 scrape_films $FN_ALL_FILMS_URLS $FN_ALL_FILMS
 
 echo +Images and Showtimes
 phantomjs map_film_info.js $FN_ALL_FILMS > $FN_FILM_INFO
+sleep $SLEEP_BETWEEN_REQUEST
 
 while read FILM_ID FILM_URL SHOWTIMES_URL
-do
+do    
     # Download image.
     FILM_URL=http://www.athinorama.gr/lmnts/events/cinema/${FILM_ID}/Poster.jpg
     echo downloading $FILM_URL
     IMG_FILE=${DIR_IMAGES}/${FILM_ID}.jpg
     curl -L $FILM_URL > $IMG_FILE
-    # Resize image to 170x250.
-    convert.exe $IMG_FILE -strip -resize 170x250 $IMG_FILE
+    # Test if downloaded file is a JPEG. Sometimes it can be an HTML file
+    # because there is no image on the site for the film.
+    file $IMG_FILE | grep -s 'JPEG'
+    if [[ $? -eq 0 ]]; then
+        # Resize image to 170x250.
+        convert $IMG_FILE -strip -resize 170x250 $IMG_FILE    
+    else
+        rm -f $IMG_FILE
+    fi    
     echo scraping showtimes ${URL_CINEMA}${SHOWTIMES_URL}
-    phantomjs scrape_film_showtimes.js $FILM_ID ${URL_CINEMA}${SHOWTIMES_URL} >> $FN_SHOWTIMES    
+    phantomjs scrape_film_showtimes.js $FILM_ID ${URL_CINEMA}${SHOWTIMES_URL} >> $FN_SHOWTIMES
+    sleep $SLEEP_BETWEEN_REQUEST
 done < $FN_FILM_INFO
 
 echo +UpdateInfo
 phantomjs prepare_update_info.js > $FN_UPDATE_INFO
 
 # Rename the tmp folder now that the process has completed successfully.
-DIR_OUT_CURR=$DIR_OUT/$(date +%Y%m%d_%H%M%S)
-mv $DIR_TMP $DIR_OUT_CURR 
+DIR_OUT_NAME=$(date +%Y%m%d_%H%M%S)
+mv $DIR_TMP $DIR_OUT/$DIR_OUT_NAME
+# Zip the out dir.
+cd $DIR_OUT
+zip -r ${DIR_OUT_NAME}.zip $DIR_OUT_NAME
