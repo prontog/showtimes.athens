55851c94ebea484590bcc983c16e9bae2142d5a9
Panos Rontogiannis <p_ronto@hotmail.com>
2017-09-17 14:48:06 +0300

    Relaxed the scraping afte occasional hicups.

--- scrape/scrape_film_showtimes.js
+++ scrape/scrape_film_showtimes.js
@@ -23,54 +23,54 @@ if (args.length !== 3) {
 
 common.scrape(url, function(ctx) {
     var id = ctx.otherArgs[0];
-    
-    var $areas = $('.piatsaname');                                
-    ctx.check($areas, '$areas');
-    
+
+    var $areas = $('.piatsaname');
+    //ctx.check($areas, '$areas');
+
     $areas.each(function() {
         var $piatsaName = $(this);
         // The area the cinema is located at.
         var area = $piatsaName.children('a').text().trim();
-        
+
         var $table = $piatsaName.next('table');
         ctx.check($table, '$table');
         while ($table[0] && $table[0].tagName === 'TABLE')
-        {            
+        {
             var $tr_first = $table.find('tr').first();
-            
+
             var $placename = $tr_first.find('a h3');
             ctx.check($placename, '$placename');
             var cinemaName = $placename.text().trim();
             var cinemaUrl = $placename.parent().attr('href');
-            
+
             var $p_address = $placename.parent().next('p');
             ctx.check($p_address, '$p_address');
             var address = $p_address.text().trim();
             var map = $p_address.find('a').attr('href');
-            
+
             var $p_phone = $p_address.next('p');
             ctx.check($p_phone, '$p_phone');
             var phone = $p_phone.text();
-            
+
             var $p_tech_info = $p_phone.next('p');
             ctx.check($p_tech_info, '$p_tech_info');
             var tech_info = $p_tech_info[0] ? $p_tech_info.text() : '';
-                        
+
             var $p_price = $p_tech_info.next('p');
-            
+
             var price = '';
             if ($p_price.length) {
                 price = $p_price.text().trim();
-            } 
+            }
             else {
                 price = tech_info;
                 tech_info = '';
             }
-            
+
             var $tr_second = $tr_first.next('tr');
             var rooms = $tr_second.text().trim();
             // ToDo: Probably should split the rooms into an array.
-            
+
             var showtime = {
                 filmId: id,
                 area: area,
@@ -83,10 +83,10 @@ common.scrape(url, function(ctx) {
                 price: price,
                 rooms: rooms
             };
-            
-            console.log(JSON.stringify(showtime));                        
-            
+
+            console.log(JSON.stringify(showtime));
+
             $table = $table.next();
         } // while table
-    }); // each .piatsaname                                                        
+    }); // each .piatsaname
 }, filmId); // scrape
